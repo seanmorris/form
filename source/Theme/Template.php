5012e5cec273361e4caf6097e70bc47a2930bfe4
Sean Morris <640101+seanmorris@users.noreply.github.com>
2022-01-17 15:23:07 -0500

    Modernizing

---
+++ source/Theme/Template.php
@@ -0,0 +1,11 @@
+<?php
+namespace SeanMorris\Form\Theme;
+/**
+ * Template for Templates.
+ */
+class Template extends \SeanMorris\Theme\View
+{
+}
+__halt_Compiler();
+?>
+<template><?=$body??'';?></template>
